a28674f1f0e8dd5d519e08ef4f08a46ea65da227
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2023-01-19 11:15:45 +0530

    Deploy

--- netflix/src/components/Header/Header.jsx
+++ netflix/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../../logo.png"
+import logo from "../../assets/logo.png"
 import {Link} from "react-router-dom"
 import {ImSearch} from 'react-icons/im'
 
